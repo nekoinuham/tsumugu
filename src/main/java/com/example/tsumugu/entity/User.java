47c49121825676b306bae9b9ab3e538386993aca
nekoinu458 <huangjingtongzhen@gmail.com>
2026-09-14 05:19:49 +0900

    initial commit

---
+++ src/main/java/com/example/tsumugu/entity/User.java
@@ -0,0 +1,44 @@
+package com.example.tsumugu.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, unique = true, length = 255)
+	private String email;
+	
+	@Column(nullable = false, length = 255)
+	private String password;
+	
+	@Column(name = "display_name", nullable = false, length = 20)
+	private String displayName;
+	
+	@Column(name = "created_at")
+	private LocalDateTime created_at;
+	
+	@Column(name = "updated_at")
+	private LocalDateTime updated_at;
+}
---
+++ src/test/java/com/example/tsumugu/TsumuguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tsumugu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsumuguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
