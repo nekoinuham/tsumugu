c70b508b6a067cdb4ee7ff46b1e133016bfc054f
nekoinuham <huangjingtongzhen+portfolio@gmail.com>
2026-09-17 09:08:12 +0900

    feat: Spring Security用のCustomUserDetailsを追加

--- src/main/java/com/example/tsumugu/security/CustomUserDetails.java
+++ src/main/java/com/example/tsumugu/security/CustomUserDetails.java
@@ -1,5 +1,57 @@
 package com.example.tsumugu.security;
 
-public class CustomUserDetails {
+import java.util.Collection;
+import java.util.List;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.tsumugu.entity.User;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
