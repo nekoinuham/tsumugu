1f95dafe2babd8e852963159d1a2abef7b23dfb6
nekoinuham <huangjingtongzhen+portfolio@gmail.com>
2026-09-17 14:37:23 +0900

    feat: CustomUserDetailsServiceを追加

--- src/main/java/com/example/tsumugu/repository/UserRepository.java
+++ src/main/java/com/example/tsumugu/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.example.tsumugu.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.tsumugu.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	
+	Optional<User> findByEmail(String email);
 }
